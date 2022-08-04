0580693c30cb7623e693fc29224e16e7211534de
iamjha007 <iamjha007@gmail.com>
2022-08-05 03:09:33 +0530

    adding feature1

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.intents
 
+import android.app.Activity
+import android.app.Notification
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.text.Editable
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.core.text.isDigitsOnly
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE_SPEECH_INPUT=100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn.setOnClickListener{
-            var newAct= Intent(this,MainActivity2::class.java)
-            newAct.putExtra("user1","Satyam")
-            startActivity(newAct)
+//        btn.setOnClickListener{
+//            var newAct= Intent(this,MainActivity2::class.java)
+//            newAct.putExtra("user1","Satyam")
+//            startActivity(newAct)
+//        }
+        micbtn.setOnClickListener{
+            speak()
         }
         emailbtn.setOnClickListener{
             val email=editText.text.toString()
@@ -43,5 +55,70 @@ class MainActivity : AppCompatActivity() {
                 startActivity(newAct)
             }
         }
+        msgbtn.setOnClickListener{
+            val number=editText.text.toString()
+            if(number.isEmpty()){
+                startWhatsapp("9899506447")
+            }else if(number.isDigitsOnly()){
+                startWhatsapp(number)
+            }else{
+                Toast.makeText(this,"Please Check the Number", Toast.LENGTH_SHORT).show()
+            }
+        }
+        youtubebtn.setOnClickListener{
+            val name=editText.text.toString();
+            val newAct=Intent()
+            newAct.action=Intent.ACTION_VIEW
+            newAct.data=Uri.parse("vnd.youtube://$name")
+            startActivity(newAct);
+
+        }
+
+    }
+
+    private fun speak() {
+       val intent=Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,Locale.getDefault())
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Speak To Type")
+        try {
+            startActivityForResult(intent,REQUEST_CODE_SPEECH_INPUT)
+        }catch (e:Exception){
+           Toast.makeText(this,e.message,Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data )
+        when(requestCode){
+            REQUEST_CODE_SPEECH_INPUT->{
+                if(resultCode== Activity.RESULT_OK && null!=data){
+                    val result=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    editText.setText(result?.get(0) ?: " didnt hear that")
+                }
+            }
+        }
+    }
+
+    private fun startWhatsapp(num: String) {
+        var number=num
+        val intent=Intent(Intent.ACTION_VIEW)
+        intent.setPackage("com.whatsapp")
+        if(number[0]=='+'){
+            number=number.substring(1)
+        }else if(number.length==10){
+            number="91"+number
+        }
+        intent.data= Uri.parse("https://wa.me/$number")
+        if(packageManager.resolveActivity(intent,0)!=null){
+            startActivity(intent)
+        }
+        else{
+            Toast.makeText(this,"whatsapp not installed",Toast.LENGTH_SHORT).show()
+        }
+        finish()
+
+
     }
 }
